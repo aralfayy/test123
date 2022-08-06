a325b0631959c220023b954bb850cc867dc67679
aralfayy <aralfayy@students.itb.ac.id>
2022-08-06 20:55:18 +0700

    aselole

--- src/Pokedex/Pokedex.js
+++ src/Pokedex/Pokedex.js
@@ -3,7 +3,7 @@ import Pokecard from "./components/Pokecard";
 import "./poke.css";
 
 const Pokedex = () => {
-  const pokemonData = `https://pokeapi.co/api/v2/pokemon?limit=5&offset=0`;
+  const pokemonData = `https://pokeapi.co/api/v2/pokemon?limit=20&offset=0`;
   const [pokelist, setPokelist] = useState([]);
   const [fetchedPokelist, setFetchedPokelist] = useState([]);
 
@@ -11,43 +11,21 @@ const Pokedex = () => {
     const res = await fetch(pokemonData);
     const data = await res.json();
 
-    function setPokelist(results) {
+    function setPokemonlist(results) {
       results.forEach(async (pokeDetails) => {
         const res = await fetch(pokeDetails.url);
         const data = await res.json();
         console.log(data);
         // append data to existing pokelist element
-        let fetchedPokeDetails = {
-          ...pokeDetails,
-          ...data,
-        };
-        setFetchedPokelist((fetchedPokelist) => [
-          ...fetchedPokelist,
-          fetchedPokeDetails,
-        ]);
+
+        setPokelist((fetchedPokeDetails) => [...fetchedPokeDetails, data]);
+        await pokelist.sort((a, b) => a.id - b.id);
       });
     }
 
-    setPokelist(data.results);
+    setPokemonlist(data.results);
+    // console.log(data.results);
   };
-
-  // const getPokeDetails = async () => {
-  //   pokelist.forEach(async (pokeDetails) => {
-
-  //     const res = await fetch(pokeDetails.url);
-  //     const data = await res.json();
-
-  //     // append data to existing pokelist element
-  //     let fetchedPokeDetails = {
-  //       ...pokeDetails,
-  //       ...data,
-  //     };
-  //     setFetchedPokelist((fetchedPokelist) => [
-  //       ...fetchedPokelist,
-  //       fetchedPokeDetails,
-  //     ]);
-  //   });
-  // };
 
   useEffect(() => {
     getPokelist();
@@ -58,20 +36,20 @@ const Pokedex = () => {
       <h1>Maybank Pokedex</h1>
       <div className="pokeContainer">
         <div className="pagination">
-          <h1>{pokelist.name}</h1>
-
-          {pokelist.map((pokemonMap, index) => (
-            <Pokecard
-              key={index}
-              id={pokemonMap.id}
-              image={pokemonMap.sprites.other.dream_world.front_default}
-              name={pokemonMap.name}
-              type={pokemonMap.types[0].type.name}
-            />
+          {pokelist.map((pokemonStats) => (
+            <div className="pokeCard">
+              <p>#0{pokemonStats.id}</p>
+              <img
+                width={80}
+                height={80}
+                src={pokemonStats.sprites.other.dream_world.front_default}
+              ></img>
+              <h3>{pokemonStats.name}</h3>
+              type={pokemonStats.types[0].type.name}
+            </div>
           ))}
         </div>
       </div>
-      {/* <button onClick={() => getPokemon()}> Load More</button> */}
     </div>
   );
 };
